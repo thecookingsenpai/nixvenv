49572ef55baa18c4b9309725eec25431d2a80d1a
Sourcery AI <>
2023-09-28 11:27:10 +0000

    'Refactored by Sourcery'

--- pip_package/src/nixvenv/__main__.py
+++ pip_package/src/nixvenv/__main__.py
@@ -27,23 +27,23 @@ def main(config_file="./config.json", new=False, delete=False):
     cmds = 'echo "[ Setting environment variables ]"\n'
     config = open(config_file, "r")
     config = json.loads(config.read())
-    print("[*] Config file loaded: " + config_file)
+    print(f"[*] Config file loaded: {config_file}")
     nvenv_name = config["nvenv_name"]
     # Halt if we have to delete the environment
     if delete:
         if not os.path.exists(nvenv_name):
-            print("[*] Environment does not exist: " + nvenv_name)
+            print(f"[*] Environment does not exist: {nvenv_name}")
         else:
             shutil.rmtree(nvenv_name, ignore_errors=True)
-            print("[*] Deleted environment: " + nvenv_name)
+            print(f"[*] Deleted environment: {nvenv_name}")
         os._exit(0)
     # Else we use the environment
-    print("[*] Reading from environment: " + nvenv_name)
-    local_bin_folder = os.getcwd() + "/" + nvenv_name + "/" + config["local_bin"]
-    print("[*] Local bin folder: " + local_bin_folder)
-    load_file = os.getcwd() + "/" + nvenv_name + "/" + config["load_file"]
+    print(f"[*] Reading from environment: {nvenv_name}")
+    local_bin_folder = f"{os.getcwd()}/{nvenv_name}/" + config["local_bin"]
+    print(f"[*] Local bin folder: {local_bin_folder}")
+    load_file = f"{os.getcwd()}/{nvenv_name}/" + config["load_file"]
     new_environment(new)
-    load_config() 
+    load_config()
     load_environment()
     
 # INFO Load a configuration file
@@ -52,16 +52,16 @@ def load_config():
     # Overwrites replace environment variables fully or create a new one
     overwrites = config['overwrites']
     for key in overwrites:
-        cmds += "export " + key + "=" + overwrites[key] + "\n"
-        print("[*] Overwriting enviromental variable: " + key)
+        cmds += f"export {key}={overwrites[key]}" + "\n"
+        print(f"[*] Overwriting enviromental variable: {key}")
     # Additions add the value to the environment variable preserving the original value
     additions = config["additions"]
     for key in additions:
-        cmds += "export " + key + "=${" + key + "}:" + additions[key] + "\n"
-        print("[*] Adding to enviromental variable: " + key)
+        cmds += f"export {key}" + "=${" + key + "}:" + additions[key] + "\n"
+        print(f"[*] Adding to enviromental variable: {key}")
     # Setting our environment variables
     print("[*] Setting our environment variables")
-    cmds += "export PATH=" + local_bin_folder + ":" + os.environ["PATH"]
+    cmds += f"export PATH={local_bin_folder}:" + os.environ["PATH"]
     # Loading the static environment variables defined in the config file
     print("[*] Setting the environment name as " + config["nvenv_name"])
     cmds += "export NVENV_NAME=" + config["nvenv_name"] + "\n"
@@ -69,9 +69,15 @@ def load_config():
     cmds += "cd " + config["working_dir"] + "\n"
     cmds += 'echo "nvenv environment loaded: $NVENV_NAME"\n'
     # Setting up customizations
-    cmds += 'export PS1="' + username + ' [' + config["nvenv_name"] + '@' + hostname+ '] :> "\n' 
+    cmds += (
+        f'export PS1="{username} ['
+        + config["nvenv_name"]
+        + '@'
+        + hostname
+        + '] :> "\n'
+    )
     # Finally, writing to the load file
-    print("[*] Writing to the load file: " + load_file)
+    print(f"[*] Writing to the load file: {load_file}")
     with open(load_file, "w") as f:
         f.write(cmds)
     
@@ -81,21 +87,21 @@ def load_environment():
     global environment, load_file, shell
     # Multiple shells are supported
     if "zsh" in shell:
-        cmd = "source " + load_file + " && " + shell + " -f -d"
+        cmd = f"source {load_file} && {shell} -f -d"
     else:
-        cmd = shell + " --rcfile " + load_file
+        cmd = f"{shell} --rcfile {load_file}"
     # Finally, loading the environment by sourcing the load file
-    print("[+] Executing: " + cmd)
+    print(f"[+] Executing: {cmd}")
     print("\nNOTE: Digit 'exit' and press ENTER to return to the previous environment\n")
     os.system(cmd)
     
 # INFO Environemnt creation
 def new_environment(new_flag=False):
     global cmds, local_bin_folder
-    if os.path.exists(os.getcwd() + "/" + nvenv_name):
+    if os.path.exists(f"{os.getcwd()}/{nvenv_name}"):
         if not new_flag:
-            raise Exception("[x] Environment already exists: " + nvenv_name)
-        print("[+] Environment already exists: " + nvenv_name)
+            raise Exception(f"[x] Environment already exists: {nvenv_name}")
+        print(f"[+] Environment already exists: {nvenv_name}")
         return
     # Creating the local_bin_folder if it doesn't exist
     if not os.path.exists(local_bin_folder):
@@ -104,21 +110,18 @@ def new_environment(new_flag=False):
     init_cmds_list = config["init_cmds"]
     for cmd in init_cmds_list:
         # Replacing folders if needed
-        if (cmd == "here"):
+        if cmd == "here":
             running_dir = os.getcwd()
-        elif (cmd == "nvenv_folder"):
-            running_dir = os.getcwd() + "/" + nvenv_name
-        elif (cmd == "local_bin_folder"):
+        elif cmd == "local_bin_folder":
             running_dir = local_bin_folder
-        else:
-            # Support for custom directories
-            # REVIEW Security note: limit it to the current working directory
-            if not os.path.exists(running_dir):
-                raise Exception("Running directory does not exist: " + running_dir)
+        elif cmd == "nvenv_folder":
+            running_dir = f"{os.getcwd()}/{nvenv_name}"
+        elif not os.path.exists(running_dir):
+            raise Exception(f"Running directory does not exist: {running_dir}")
         # Compiling the command to be executed before the environment is activated
         single_cmd = init_cmds_list[cmd]
-        actual_cmd = "cd '" + running_dir + "' && " + single_cmd
-        print("[*] Executing: " + actual_cmd)
+        actual_cmd = f"cd '{running_dir}' && {single_cmd}"
+        print(f"[*] Executing: {actual_cmd}")
         os.system(actual_cmd)
     # Support files
     files = config["files"]
@@ -126,10 +129,10 @@ def new_environment(new_flag=False):
         origin = file[0]
         destination = file[1]
         if not os.path.exists(origin):
-            raise Exception("[x] Origin file does not exist: " + origin)
+            raise Exception(f"[x] Origin file does not exist: {origin}")
         if os.path.exists(destination):
-            raise Exception("[x] Destination file already exists: " + destination)
-        print("[*] Copying file: " + origin + " to: " + destination)
+            raise Exception(f"[x] Destination file already exists: {destination}")
+        print(f"[*] Copying file: {origin} to: {destination}")
         shutil.copy(origin, destination)
 
 if __name__ == '__main__':
@@ -151,7 +154,7 @@ if __name__ == '__main__':
     if not os.path.exists(params["config_file"]):
         raise Exception("[x] Config file does not exist: " + params["config_file"])
     # Runs based on operation
-    if params["operation"] == "activate" or params["operation"] == "run":
+    if params["operation"] in ["activate", "run"]:
         main(config_file=params["config_file"])
     elif params["operation"] == "new":
         main(config_file=params["config_file"], new=True)
